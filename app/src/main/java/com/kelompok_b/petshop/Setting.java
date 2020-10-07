fb505479c863dff6201f38e4ae793fc39a3be58e
KalebPuniwan <Kalebpuniwan19@gmail.com>
2020-10-07 23:25:24 +0700

    Setting Dark Mode

---
+++ app/src/main/java/com/kelompok_b/petshop/Setting.java
@@ -0,0 +1,32 @@
+package com.kelompok_b.petshop;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+
+public class setting extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+
+    }
+
+    public void btnNightMode(View view){
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        startActivity(new Intent(getApplicationContext(),MainActivity.class));
+        finish();
+    }
+
+    public void btnLightMode(View view){
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        startActivity(new Intent(getApplicationContext(),MainActivity.class));
+        finish();
+    }
+}
